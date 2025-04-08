8dbbe05d1e65bdfba63c9f08759f51a6188b9af0
Alvaro Peña <me@alvropena.com>
2025-04-08 10:02:59 -0700

    Add toast component with provider and toaster functionality

--- components/UserActivitySection.tsx
+++ components/UserActivitySection.tsx
@@ -1,5 +1,4 @@
 import { RecentSessions } from "./RecentSessions";
-import { UserAnalytics } from "./UserAnalytics";
 
 type UserActivitySectionProps = {
   isLoading?: boolean;
@@ -8,9 +7,8 @@ type UserActivitySectionProps = {
 export function UserActivitySection({ isLoading = false }: UserActivitySectionProps) {
   return (
     <div className="mb-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="w-full">
         <RecentSessions isLoading={isLoading} />
-        <UserAnalytics />
       </div>
     </div>
   );
