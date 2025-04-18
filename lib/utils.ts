5d61793c632bfec4323a04f0c416f6174f4c7879
Alvaro Peña <me@alvropena.com>
2025-04-18 09:48:53 -0700

    Add dark mode support and update tailwind-merge version

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function formatCurrency(amount: number, currency = 'usd') {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currency.toUpperCase(),
-    minimumFractionDigits: 2,
-  });
-
-  return formatter.format(amount / 100); // Stripe amounts are in cents
 }
