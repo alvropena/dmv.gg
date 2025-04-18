9fe4f2ed62f0422335428e151849fecaf8d7b6b5
Alvaro Peña <me@alvropena.com>
2025-04-18 09:51:59 -0700

    Add formatCurrency function for currency formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number, currency: string = 'USD'): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency.toUpperCase(),
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount / 100) // Assuming amount is in cents
+}
